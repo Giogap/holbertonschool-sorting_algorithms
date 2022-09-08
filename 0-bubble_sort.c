3d5f886cf0097cbd1757e05de4c99efe076ee825
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-07 23:28:20 -0500

    update 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,4 +26,16 @@ void bubble_sort(int *array, size_t size)
 
 	if (size == 0)
 		return;
+
+	for (i = 0; i < size - 1; i++)			/* n  */
+	{
+		for (j = 0; j < size - i - 1; j++)	/* n^2 */
+		{
+			if (array[j] > array[j + 1])	/* n^2 */
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
