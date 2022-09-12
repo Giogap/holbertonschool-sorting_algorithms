1ed81569da3090401697dac4c283b863fb46a78b
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-12 15:26:36 -0500

    update 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,9 @@
 #include "sort.h"
 
 /**
- *
+ * selection_sort - check code
+ * @array: pointer array
+ * @size: size to array
  */
 
 void selection_sort(int *array, size_t size)
@@ -11,16 +13,17 @@ void selection_sort(int *array, size_t size)
 	if (size == 0)
 		return;
 
-	/* ordenar a[0]..a[n-2] y a[n-1] en cada pasada */
-	for (i = 0; i < size - 1; i++)						/* n^2 */
+	for (i = 0; i < size - 1; i++)
 	{
-		/* Comienzo de la exploración en índice i */
 		indiceMenor = i;
-		/* J explora la sublista a[i+1]...a[n-1] */
-		for (j = i + 1; j < size; j++)					/* n^2 */
-			if (array[j] < array[indiceMenor])			/* n^2 */
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[indiceMenor])
+			{
 				indiceMenor = j;
-				/* situa el elemente mas pequeño en a[i] */
+			}
+		}
 		if (i != indiceMenor)
 		{
 			double aux = array[i];
@@ -28,7 +31,6 @@ void selection_sort(int *array, size_t size)
 			array[i] = array[indiceMenor];
 			array[indiceMenor] = aux;
 			print_array(array, size);
-
 		}
 	}
 }
