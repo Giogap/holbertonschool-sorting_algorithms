406c23b800204d7a6fbf5bb39e8c548624418466
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-12 14:30:17 -0500

    s.h

--- sort.h
+++ sort.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -25,7 +24,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-
 
 #endif /* SORT_H */
